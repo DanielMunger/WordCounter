062bb5b713a45f64c00dcd5c49f897d1bdb80282
Daniel Munger <mungerda@gmail.com>
2016-12-02 09:45:24 -0800

    add test method for first spec.

--- Tests/WordFinderTests.cs
+++ Tests/WordFinderTests.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using WordCounter.Objects;
+//using WordCounter.Objects;
 using Xunit;
 
 namespace WordCounterTests
@@ -11,51 +11,19 @@ namespace WordCounterTests
       public void WordCounter_MatchingSingleLetter_One()
       {
         //Arrange
+        string _userWord = "a";
+        string _userSentece = "";
+        int _instances = 0;
+        int _expected = 1;
         //Action
+        if(_userWord == _userSentece)
+        {
+          _instances += 1;
+        }
         //Assert
+        Assert.Equal(true, _instances == _expected);
       }
-      [Fact]
-      public void WordCounter_MatchingMultipleLetters_One()
-      {
-        //Arrange
-        //Action
-        //Assert
-      }
-      [Fact]
-      public void WordCounter_MatchingMultipleLettersAnywhere_One()
-      {
-        //Arrange
-        //Action
-        //Assert
-      }
-      [Fact]
-      public void WordCounter_DifferentCharacters_One()
-      {
-        //Arrange
-        //Action
-        //Assert
-      }
-      [Fact]
-      public void WordCounter_DifferentCharactersAnywhere_One()
-      {
-        //Arrange
-        //Action
-        //Assert
-      }
-      [Fact]
-      public void WordCounter_DifferentCharactersMultiple_Two()
-      {
-        //Arrange
-        //Action
-        //Assert
-      }
-      [Fact]
-      public void WordCounter_CaseInsensitive_Two()
-      {
-        //Arrange
-        //Action
-        //Assert
-      }
+    
 
   }
 }
